1172788b0d1b81ecb79bbff7879b1545bc2f8de8
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-01-23 10:27:14 +1100

    Format script document

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 projectButtons = document.querySelectorAll(".project-buttons");
-trendingAccounts = document.querySelectorAll(".trending-accounts")
-
+trendingAccounts = document.querySelectorAll(".trending-accounts");
 
 projectButtons.forEach((button) => {
-    function createButton(link, altName) {
-        let button = document.createElement("img");
-        button.setAttribute('src', link);
-        button.setAttribute('alt', altName)
-        return button;
-    };
-    button.appendChild(createButton('icons/heart-outline.svg', 'favourite-icon'));
-    button.appendChild(createButton('icons/comment-plus-outline.svg', 'comment-icon'));
-    button.appendChild(createButton('icons/source-fork.svg', 'fork-icon'));
+  function createButton(link, altName) {
+    let button = document.createElement("img");
+    button.setAttribute("src", link);
+    button.setAttribute("alt", altName);
+    return button;
+  }
+  button.appendChild(createButton("icons/heart-outline.svg", "favourite-icon"));
+  button.appendChild(createButton("icons/comment-plus-outline.svg", "comment-icon"));
+  button.appendChild(createButton("icons/source-fork.svg", "fork-icon"));
 });
 
 trendingAccounts.forEach((account) => {
-    function createButton(link, altName) {
-        let button = document.createElement("img");
-        button.setAttribute('src', link);
-        button.setAttribute('alt', altName)
-        return button;
-    };
-    account.appendChild(createButton('icons/account-circle-outline.svg', 'account-icon'))
-})
+  function createButton(link, altName) {
+    let button = document.createElement("img");
+    button.setAttribute("src", link);
+    button.setAttribute("alt", altName);
+    return button;
+  }
+  account.appendChild(
+    createButton("icons/account-circle-outline.svg", "account-icon")
+  );
+});
