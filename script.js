13d2faa64286f0644be621d3a9d91d933bbfd403
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-01-22 15:34:10 +1100

    Add and format trending elements

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 projectButtons = document.querySelectorAll(".project-buttons");
+trendingAccounts = document.querySelectorAll(".trending-accounts")
 
 
 projectButtons.forEach((button) => {
@@ -12,3 +13,13 @@ projectButtons.forEach((button) => {
     button.appendChild(createButton('icons/comment-plus-outline.svg', 'comment-icon'));
     button.appendChild(createButton('icons/source-fork.svg', 'fork-icon'));
 });
+
+trendingAccounts.forEach((account) => {
+    function createButton(link, altName) {
+        let button = document.createElement("img");
+        button.setAttribute('src', link);
+        button.setAttribute('alt', altName)
+        return button;
+    };
+    account.appendChild(createButton('icons/account-circle-outline.svg', 'account-icon'))
+})
